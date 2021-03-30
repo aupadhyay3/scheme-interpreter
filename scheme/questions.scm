9ce9297030701766110828dd2ac6d32f73fd4c4b
Aastha Upadhyay <sendmail2aastha@gmail.com>
2021-03-29 19:36:02 -0700

    moved project files to repo

---
+++ scheme/questions.scm
@@ -0,0 +1,64 @@
+(define (caar x) (car (car x)))
+(define (cadr x) (car (cdr x)))
+(define (cdar x) (cdr (car x)))
+(define (cddr x) (cdr (cdr x)))
+
+
+;; Problem 15
+;; Returns a list of two-element lists
+(define (enumerate s)
+  ; BEGIN PROBLEM 15
+  (define (helper index s)
+      (if (null? s) 
+          s
+          (cons (list index (car s)) (helper (+ 1 index) (cdr s)))
+      )
+  )
+  (helper 0 s)
+ )
+; END PROBLEM 15
+
+;; Problem 16
+
+;; Merge two lists LIST1 and LIST2 according to COMP and return
+;; the merged lists.
+(define (merge comp list1 list2)
+  ; BEGIN PROBLEM 16
+  (cond ((null? list1) list2)
+        ((null? list2) list1)
+        ((comp (car list1) (car list2)) (cons (car list1) (merge comp (cdr list1) list2)) )
+        (else (cons (car list2) (merge comp list1 (cdr list2))) )
+  )
+)
+
+  ; END PROBLEM 16
+
+
+(merge < '(1 5 7 9) '(4 8 10))
+; expect (1 4 5 7 8 9 10)
+(merge > '(9 7 5 1) '(10 8 4 3))
+; expect (10 9 8 7 5 4 3 1)
+
+;; Problem 17
+(define (reverse s)
+    (if (null? s) nil
+         (append (reverse (cdr s)) (list (car s)))
+        )
+)
+
+(define (nondecreaselist s)
+    ; BEGIN PROBLEM 17
+    (define (helper s ongoing)
+        (cond ((null? s) (list (reverse ongoing)))
+              ((or (null? ongoing) (>= (car s) (car ongoing))) 
+                    (helper (cdr s) (cons (car s) ongoing)))
+                (else
+                    (cons (reverse ongoing) (helper s nil)))
+        )
+    )
+(helper s nil)
+)
+    ; END PROBLEM 17
+
+
+
